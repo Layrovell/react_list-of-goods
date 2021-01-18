806d1f1c49c8999f749bcf951002b7445d4a4a3f
Layrovell <marina95prodan@gmail.com>
2021-01-19 00:04:06 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.9da7e04b16def911fdce1df3c85b1d22.js"
 );
 
 self.addEventListener('message', (event) => {
